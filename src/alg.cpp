8a745146a98201c5be23ba45c27ab5dbcb830192
Sashaperepel <124892766+Sashaperepel@users.noreply.github.com>
2023-02-18 14:07:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,18 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int nod = 1;
+    int min = 0;
+    if (a < b) {
+        min = a;
+    }
+    else {
+        min = b;
+    }
+    for (int i = 1; i <= min; ++i) {
+        if ((a % i == 0) && (b % i == 0)) {
+            nod = i;
+        }
+    }
+    return nod;
 }
